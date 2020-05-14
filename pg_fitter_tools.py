3b8f95eb2988bd1738228cb466f7cabbdb5d3dd7
Nick Prouse <nprouse@triumf.ca>
2020-05-14 16:20:44 -0700

    Use dictionary instead of array for image feature locations so features/images can be indexed by name. Change constructor to allow distortion parameters  to be passed for working with distorted images.

--- pg_fitter_tools.py
+++ pg_fitter_tools.py
@@ -4,21 +4,33 @@ from scipy import linalg
 import cv2
 import csv
 
-
-class photogrammetry_fitter:
-    def __init__(self, image_feature_locations, seed_feature_locations, focal_length, principle_point):
-        self.image_feature_locations = image_feature_locations
-        self.seed_feature_locations = seed_feature_locations
-        self.nimages = image_feature_locations.shape[0]
-        self.nfeatures = image_feature_locations.shape[1]
-        assert image_feature_locations.shape[2] == 2
-        assert seed_feature_locations.shape[0] == self.nfeatures
-        assert seed_feature_locations.shape[1] == 3
-        self.camera_matrix = np.array([
-            [focal_length[0], 0, principle_point[0]],
-            [0, focal_length[1], principle_point[1]],
-            [0, 0, 1]])
-        self.distortion = np.zeros((4, 1))
+class PhotogrammetryFitter:
+    def __init__(self, image_feature_locations, seed_feature_locations, focal_length, principle_point,
+                 radial_distortion=(0., 0.), tangential_distortion=(0., 0.)):
+        self.nimages = len(image_feature_locations)
+        self.nfeatures = len(seed_feature_locations)
+        self.seed_feature_locations = np.zeros((self.nfeatures, 3))
+        self.image_feature_locations = np.zeros((self.nimages, self.nfeatures, 2))
+        self.feature_index = {}
+        self.index_feature = {}
+        f_index = 0
+        for f_key, f in seed_feature_locations.items():
+            self.feature_index[f_key] = f_index
+            self.index_feature[f_index] = f_key
+            self.seed_feature_locations[f_index] = f
+            f_index += 1
+        self.image_index = {}
+        self.index_image = {}
+        i_index = 0
+        for i_key, i in image_feature_locations.items():
+            self.image_index[i_key] = i_index
+            self.index_image[i_index] = i_key
+            for f_key, f in i.items():
+                f_index = self.feature_index[f_key]
+                self.image_feature_locations[i_index, f_index] = f
+            i_index += 1
+        self.camera_matrix = build_camera_matrix(focal_length, principle_point)
+        self.distortion = np.concatenate((radial_distortion, tangential_distortion)).reshape((4,1))
 
     def estimate_camera_poses(self):
         camera_rotations = np.zeros((self.nimages, 3))
@@ -31,10 +43,10 @@ class photogrammetry_fitter:
                 self.camera_matrix, self.distortion, flags=cv2.SOLVEPNP_ITERATIVE)
             if not success:
                 print("FAILED to find camera pose: camera", i)
-            R = cv2.Rodrigues(rotation_vector)[0]
-            if np.mean(R.dot(self.seed_feature_locations.transpose())[2] + translation_vector[2]) < 0:
-                rot_mat = [-1, -1, 1] * cv2.Rodrigues(rotation_vector)[0]
-                rotation_vector = cv2.Rodrigues(rot_mat)[0].squeeze()
+            rotation_matrix = cv2.Rodrigues(rotation_vector)[0]
+            if np.mean(rotation_matrix.dot(self.seed_feature_locations.transpose())[2] + translation_vector[2]) < 0:
+                rotation_matrix = [-1, -1, 1] * cv2.Rodrigues(rotation_vector)[0]
+                rotation_vector = cv2.Rodrigues(rotation_matrix)[0].squeeze()
                 translation_vector = -translation_vector
             reprojected = cv2.projectPoints(self.seed_feature_locations[indices], rotation_vector, translation_vector,
                                             self.camera_matrix, self.distortion)[0].reshape((indices.size, 2))
@@ -72,6 +84,7 @@ class photogrammetry_fitter:
         camera_rotations = res.x[:self.nimages * 3].reshape((-1, 3))
         camera_translations = res.x[self.nimages * 3:self.nimages * 6].reshape((-1, 3))
         reco_locations = res.x[self.nimages * 6:].reshape((-1, 3))
+        reco_locations = {f : reco_locations[i] for f, i in self.feature_index.items()}
         return camera_rotations, camera_translations, reco_locations
 
     def fit(self):
@@ -104,14 +117,16 @@ def project_points(points, camera_params):
 
 
 def kabsch_errors(true_feature_locations, reco_feature_locations):
-    translation = reco_feature_locations.mean(axis=0)
-    reco_translated = reco_feature_locations - translation
-    true_translated = true_feature_locations - true_feature_locations.mean(axis=0)
+    true_location_matrix = np.array(list(true_feature_locations.values()))
+    reco_location_matrix = np.array(list(reco_feature_locations.values()))
+    translation = reco_location_matrix.mean(axis=0)
+    reco_translated = reco_location_matrix - translation
+    true_translated = true_location_matrix - true_location_matrix.mean(axis=0)
     C = true_translated.transpose().dot(reco_translated)/reco_translated.shape[0]
     U, D, V = linalg.svd(C)
     S = np.eye(3)
     if linalg.det(U)*linalg.det(V) < 0:
-        S[2,2] = -1
+        S[2, 2] = -1
     R = U.dot(S).dot(V)
     scale = (D*S).trace()/reco_translated.var(axis=0).sum()
     reco_transformed = scale*R.dot(reco_translated.transpose()).transpose()
@@ -120,13 +135,13 @@ def kabsch_errors(true_feature_locations, reco_feature_locations):
 
 
 def camera_orientations(camera_rotations):
-    rot_mat = np.array([cv2.Rodrigues(r)[0] for r in camera_rotations])
-    return rot_mat.transpose((0,2,1))
+    rotation_matrices = np.array([cv2.Rodrigues(r)[0] for r in camera_rotations])
+    return rotation_matrices.transpose((0, 2, 1))
 
 
-def camera_poses(camera_rotations, camera_translations):
+def camera_world_poses(camera_rotations, camera_translations):
     orientations = camera_orientations(camera_rotations)
-    positions = np.matmul(orientations, -camera_translations.reshape((-1,3,1))).squeeze()
+    positions = np.matmul(orientations, -camera_translations.reshape((-1, 3, 1))).squeeze()
     return orientations, positions
 
 
