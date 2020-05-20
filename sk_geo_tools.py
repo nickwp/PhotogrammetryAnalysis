23f04b508139081897bbc1e9aa854a5a22bd6e47
Nick Prouse <nprouse@triumf.ca>
2020-05-20 00:40:07 -0700

    fix bug in bolt numbering

--- sk_geo_tools.py
+++ sk_geo_tools.py
@@ -13,9 +13,9 @@ def get_bolt_locations_barrel(pmt_locations):
             continue
         phi = np.arctan2(pmt[1], pmt[0])
         bolt_locations.update({
-            re.sub(r"-00$", "-"+str(i).zfill(2), f): np.array([
+            re.sub(r"-00$", "-"+str(i+1).zfill(2), f): np.array([
                 pmt[0] - pmt_bolt_offset*np.cos(phi) + bolt_ring_radius*np.sin(i*np.pi/12.)*np.sin(phi),
                 pmt[1] - pmt_bolt_offset*np.sin(phi) - bolt_ring_radius*np.sin(i*np.pi/12.)*np.cos(phi),
                 pmt[2] + bolt_ring_radius*np.cos(i*np.pi/12.)])
-            for i in range(1, 25)})
+            for i in range(0, 24)})
     return bolt_locations
